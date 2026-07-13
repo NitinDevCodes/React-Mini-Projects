a95bc72e485664d593ac37ccce1f80c5122ac69d
NitinDevCodes <contactnitin.inbox@gmail.com>
2026-07-14 00:15:27 +0530

    Added PostForm

--- Appwrite-Blog/src/components/index.js
+++ Appwrite-Blog/src/components/index.js
@@ -4,6 +4,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE"
 
 export {
     Header,
@@ -11,4 +12,5 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    RTE
 }
